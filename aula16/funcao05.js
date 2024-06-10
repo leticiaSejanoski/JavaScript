38dbe0cdfd4a554c0f7df20e845798fd33c1be04
leticiaSejanoski <131303547+leticiaSejanoski@users.noreply.github.com>
2024-06-10 17:43:29 -0300

    Curso finalizado!

---
+++ aula16/funcao05.js
@@ -0,0 +1,17 @@
+
+/*RECURSIVIDADE*/
+
+function fatorial(n){
+     if(n == 1){
+        return 1
+     }else{
+        return n* fatorial(n-1)
+     }
+}
+console.log(fatorial(6))
+
+/*
+n! = n (n-1)!
+1! = 1
+
+*/
